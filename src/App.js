540d80b58510ae3cc360412de337af28063fb934
Muhammad Abbas Khan <100797911+abbaskhan349@users.noreply.github.com>
2022-09-28 15:35:33 +0500

    api..

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
-import ConfirmPassword from './pages/ConfirmPassword';
-import ForgotPassword from './pages/ForgotPassword';
 import Login from './pages/Login';
-import Register from './pages/Register';
-import ResetPassword from './pages/ResetPassword';
 
-function App() {
+function App() { 
+  
   return (
     <div className="App">
       <Login />
-      {/* <Register /> */}
-      {/* <ForgotPassword /> */}
-      {/* <ResetPassword /> */}
-      {/* <ConfirmPassword /> */}
     </div>
   );
 }
